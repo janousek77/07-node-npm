81698cb9219b115aa748a8199b0caba0d0553c30
janousek77 <janousek77@gmail.com>
2017-04-19 09:13:00 -0700

    completed second todo

--- chris-bret/server.js
+++ chris-bret/server.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// TODO: Initialize your project using NPM to create and populate a package.json file
-
 // TODO: Require the Express package that you installed via NPM, and instantiate the app
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
 // There is also a package here called body-parser, which is required in for use in a new route.
